a5ca08a59db24fa63fb6c5f598852b6389c1df38
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2023-11-27 12:07:12 +0300

    update

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
-import { motion } from "framer-motion";
-
-
 import Rating from "@mui/material/Rating";
 import Chip from "@mui/material/Chip";
 
@@ -11,7 +8,6 @@ import SellIcon from "@mui/icons-material/Sell";
 import Count from "./QuantityButtons";
 import ButtonGroup from "./ButtonGroup";
 import Basket from "../Basket";
-
 
 import offerFruits from "../../assets/images/offerfruits.png";
 import lemon from "../../assets/images/lemon.png";
@@ -26,11 +22,7 @@ function index({ product }) {
   );
 
   return (
-    <motion.div
-    initial={{ opacity: 0, y: 20 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{type: 'spring', duration: 0.3 }}
-    className="w-full h-full bg-gray-200 rounded-3xl p-5 transition-all duration-300 group cursor-pointer">
+    <div className="w-full h-full bg-gray-200 rounded-3xl p-5 transition-all duration-300 group cursor-pointer">
       <div className="w-full h-[19rem] bg-white flex flex-col items-center justify-center rounded-2xl relative">
         <Chip
           icon={<SellIcon className="text-lg !ml-2" />}
@@ -79,7 +71,7 @@ function index({ product }) {
         <Count product={product} />
         <Basket open={openBasket} setOpenBasket={setOpenBasket} />
       </div>
-    </motion.div>
+    </div>
   );
 }
 
