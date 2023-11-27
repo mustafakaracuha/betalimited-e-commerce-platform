916ad532a2a7d58fa0dddc31dd8b538d3f321514
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2023-11-27 10:54:10 +0300

    update

--- src/pages/Home/NotFound/index.jsx
+++ src/pages/Home/NotFound/index.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 
-import Banana from '../../../assets/images/banana.png'
+import Banana from "../../../assets/images/banana.png";
 
 function index() {
   return (
-    <div className="w-full h-screen flex flex-col items-center justify-center">
-     <img width={80} src={Banana} className='mb-5'/>
-    <p className='text-lg font-medium'>This product not found</p>
-  </div>
-  )
+    <motion.div
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ type: "spring", duration: 0.5 }}
+      className="w-full h-screen flex flex-col items-center justify-center"
+    >
+      <img width={80} src={Banana} className="mb-5" />
+      <p className="text-lg font-medium">This product not found</p>
+    </motion.div>
+  );
 }
 
-export default index
+export default index;
