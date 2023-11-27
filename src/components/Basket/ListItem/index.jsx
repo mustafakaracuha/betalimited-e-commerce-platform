9b045c4b88823fa34a88672d3ab3ba45d266994c
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2023-11-27 10:51:35 +0300

    update

--- src/components/Basket/ListItem/index.jsx
+++ src/components/Basket/ListItem/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { motion } from "framer-motion";
 
 import ListItem from "@mui/material/ListItem";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { CircularProgress } from "@mui/material";
 
 import offerFruits from "../../../assets/images/offerfruits.png";
 import lemon from "../../../assets/images/lemon.png";
@@ -19,7 +19,7 @@ function index({ product }) {
 
   const calculateTotalPrice = (products) => {
     return products?.reduce((total, product) => {
-      return (product.quantity * product.price) + total;
+      return product.quantity * product.price + total;
     }, 0);
   };
 
@@ -48,50 +48,57 @@ function index({ product }) {
 
   return (
     <ListItem disablePadding key={product.id}>
-      <div className="w-full h-26 bg-gray-100 p-3 rounded-xl mt-5 group cursor-pointer transition-all duration-300 hover:bg-blue-100 hover:shadow-xl hover:shadow-gray-100 relative">
-        <div className="w-full flex items-center justify-between px-2">
-          <div className="flex items-center">
-            <div className="w-20 h-20 flex items-center justify-center rounded-xl bg-white">
-              <img
-                className="transition-all duration-300 group-hover:rotate-12 group-hover:scale-110"
-                src={
-                  product.name === "Offer Fruits"
-                    ? offerFruits
-                    : product.name === "Lemon"
-                    ? lemon
-                    : product.name === "Fresh Strawberry"
-                    ? strawberry
-                    : ""
-                }
-                width={
-                  product.name === "Offer Fruits"
-                    ? 140
-                    : product.name === "Fresh Strawberry"
-                    ? 60
-                    : 60
-                }
-              />
+      <motion.div
+        className="w-full"
+        initial={{ opacity: 0, y: -10 }}
+        animate={{ opacity: 1, y: 10 }}
+        transition={{ type: "spring", duration: 0.5, delay: 0.3 }}
+      >
+        <div className="w-full h-26 bg-gray-100 p-3 rounded-xl mt-5 group cursor-pointer transition-all duration-300 hover:bg-blue-100 hover:shadow-xl hover:shadow-gray-100 relative">
+          <div className="w-full flex items-center justify-between px-2">
+            <div className="flex items-center">
+              <div className="w-20 h-20 flex items-center justify-center rounded-xl bg-white">
+                <img
+                  className="transition-all duration-300 group-hover:rotate-12 group-hover:scale-110"
+                  src={
+                    product.name === "Offer Fruits"
+                      ? offerFruits
+                      : product.name === "Lemon"
+                      ? lemon
+                      : product.name === "Fresh Strawberry"
+                      ? strawberry
+                      : ""
+                  }
+                  width={
+                    product.name === "Offer Fruits"
+                      ? 140
+                      : product.name === "Fresh Strawberry"
+                      ? 60
+                      : 60
+                  }
+                />
+              </div>
+              <div>
+                <p className="ml-4 text-lg">{product.name}</p>
+                <p className="ml-4 text-gray-400">
+                  {"Quantity:" + " " + product.quantity}
+                </p>
+              </div>
             </div>
-            <div>
-              <p className="ml-4 text-lg">{product.name}</p>
-              <p className="ml-4 text-gray-400">
-                {"Quantity:" + " " + product.quantity}
-              </p>
-            </div>
-          </div>
-          <p>
-            ${product.price}
-            {product.quantity >= 1 && (
-              <button
-                onClick={() => handeDeleteMyProduct(product)}
-                className="w-10 h-10 bg-rose-100 transition-all duration-300 hover:bg-rose-200 flex items-center justify-center p-1 rounded-xl absolute -top-3 -right-3 ring-4 ring-white"
-              >
+            <p>
+              ${product.price}
+              {product.quantity >= 1 && (
+                <button
+                  onClick={() => handeDeleteMyProduct(product)}
+                  className="w-10 h-10 bg-rose-100 transition-all duration-300 hover:bg-rose-200 flex items-center justify-center p-1 rounded-xl absolute -top-3 -right-3 ring-4 ring-white"
+                >
                   <DeleteIcon className="!text-[1.3rem] text-rose-400" />
-              </button>
-            )}
-          </p>
+                </button>
+              )}
+            </p>
+          </div>
         </div>
-      </div>
+      </motion.div>
     </ListItem>
   );
 }
